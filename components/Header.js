972f51f358fce5b6454ee5260a87bfff03ef058a
Malcev-Nikita <nik2004itamalcev@gmail.com>
2023-05-19 00:00:08 +0400

    commit 19.5.23 (1)

---
+++ components/Header.js
@@ -0,0 +1,16 @@
+const Header = () => (
+    <nav className="nav">
+        <div className="logo"><h2>LOGO</h2></div>
+
+        <ul className="nav__ul">
+            <a href="#"><li>ГЛАВНАЯ</li></a>
+            <a href="#"><li>О НАС</li></a>
+            <a href="#"><li>УСЛУГИ</li></a>
+            <a href="#"><li>ПОРТФОЛИО</li></a>
+            <a href="#"><li>КЛИЕНТЫ</li></a>
+            <a href="#"><li>КОНТАКТЫ</li></a>
+        </ul>
+    </nav>
+);
+
+export default Header;
